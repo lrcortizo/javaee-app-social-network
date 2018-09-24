58ebd6fe1544a88389fc560c363d3833452c178a
lrcortizo <lrcortizo@esei.uvigo.es>
2018-09-24 21:23:08 +0200

    Comment entity created

---
+++ domain/src/main/java/es/uvigo/esei/dgss/exercises/domain/Comment.java
@@ -0,0 +1,47 @@
+package es.uvigo.esei.dgss.exercises.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String comment;
+	private Date date;
+	
+	public Comment(){
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
