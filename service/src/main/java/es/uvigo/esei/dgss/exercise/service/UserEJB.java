0bee80f2ba847f51e2922f9bebabe0ff557ff706
lrcortizo <lrcortizo@esei.uvigo.es>
2018-09-26 17:40:28 +0200

    UserEJB: find, update and remove users

--- service/src/main/java/es/uvigo/esei/dgss/exercise/service/UserEJB.java
+++ service/src/main/java/es/uvigo/esei/dgss/exercise/service/UserEJB.java
@@ -12,8 +12,26 @@ public class UserEJB {
 	@PersistenceContext
 	private EntityManager em;
 	
+	public User findUserById(String login) {
+		return em.find(User.class, login);
+	}
+	
 	public User createUser(User user){
 		em.persist(user);
 		return user;
 	}
+	
+	public User updateUser(String login, String name, String password, byte[] picture){
+		User user = findUserById(login);
+		user.setName(name);
+		user.setPassword(password);
+		user.setPicture(picture);
+		em.persist(user);
+		return user;
+	}
+	
+	public void removeUser(User user){
+		em.remove(user);
+	}
+	
 }
