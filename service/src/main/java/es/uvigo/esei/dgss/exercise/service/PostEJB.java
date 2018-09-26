951d54b8b7a28e31ee5c2f603ff93478ca0ef2ce
lrcortizo <lrcortizo@esei.uvigo.es>
2018-09-26 18:01:18 +0200

    PostEJB: find, update and remove posts/ not finished, missing specify post type

--- service/src/main/java/es/uvigo/esei/dgss/exercise/service/PostEJB.java
+++ service/src/main/java/es/uvigo/esei/dgss/exercise/service/PostEJB.java
@@ -1,5 +1,7 @@
 package es.uvigo.esei.dgss.exercise.service;
 
+import java.util.Date;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,9 +14,24 @@ public class PostEJB {
 	@PersistenceContext
 	private EntityManager em;
 	
+	public Post findPostById(int id) {
+		return em.find(Post.class, id);
+	}
+	
 	public Post createPost(Post post){
 		em.persist(post);
 		return post;
+	}
+	
+	public Post updatePost(int id, Date date){
+		Post post = findPostById(id);
+		post.setDate(date);
+		em.persist(post);
+		return post;
+	}
+	
+	public void removePost(Post Post){
+		em.remove(Post);
 	}
 }
 
