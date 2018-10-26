9dc2d38fb405ecadac921a4ba738bb1f9cc60a7e
Luis Miguel Raña Cortizo <lrcortizo@esei.uvigo.es>
2018-10-26 13:43:57 +0200

    EmailServiceEJB(not working yet) TODO: manage session and create user email

---
+++ service/src/main/java/es/uvigo/esei/dgss/exercise/service/EmailServiceEJB.java
@@ -0,0 +1,37 @@
+package es.uvigo.esei.dgss.exercise.service;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import es.uvigo.esei.dgss.exercises.domain.User;
+ 
+@Stateless
+public class EmailServiceEJB {
+ 
+    @Resource(name = "java:jboss/mail/gmail")
+    private Session session;
+ 
+    public void send(User u, String subject, String body) {
+ 
+        try {
+ 
+            Message message = new MimeMessage(session);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(u.email));
+            message.setSubject(subject);
+            message.setText(body);
+ 
+            Transport.send(message);
+ 
+        } catch (MessagingException e) {
+            Logger.getLogger(EmailServiceEJB.class.getName()).log(Level.WARNING, "Cannot send mail", e);
+        }
+    }
+}
